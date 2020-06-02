ab7b39e08b9fa962d80879a8440e99657a03976b
ddonadelli <davi_donadelli@live.com>
2020-06-02 19:27:04 -0300

    Estrutura do projeto

---
+++ src/main/java/br/com/codenation/criptografia/Criptografia.java
@@ -0,0 +1,13 @@
+package br.com.codenation.criptografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Criptografia {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(Criptografia.class, args);
+	}
+
+}
